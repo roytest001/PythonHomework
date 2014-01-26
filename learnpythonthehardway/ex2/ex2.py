cb28e1ef29ac463b541b6d380f9efcb0cd7e919d
roytest001 <xxjbs001@126.com>
2014-01-26 22:58:18 +0800

    add three new files for the PythonHomework

---
+++ learnpythonthehardway/ex2/ex2.py
@@ -0,0 +1,7 @@
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by python.
+print "I coudl have code like this." # and the comment after is ignored
+
+#you can also user a comment to "disable" or comment out a piece of code:
+print "This will run."
+print "This won't run"
